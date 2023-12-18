ad018ea16f8ba3e6700fd2cd3b94b96a20a947b3
Scriptscrypt <tirumaladasa@gmail.com>
2023-12-18 16:37:04 +0530

    Setup Codemagic #1

--- lib/config/dotenv_config.dart
+++ lib/config/dotenv_config.dart
@@ -1,18 +1,23 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+// With prefix (optional)
+import 'dart:io' as io;
+
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-late String supabaseUrl;
-late String supabaseKey;
+String? supabaseUrl;
+String? supabaseKey;
 
 Future<void> loadEnv() async {
-  if (dotenv.env['dotenv_filename'] != '') {
-    await dotenv.load(fileName: 'assets/.env');
-  }
+  // if (dotenv.env['dotenv_filename'] != '') {
+  //   await dotenv.load(fileName: 'assets/.env');
+  // }
 
-  supabaseUrl = dotenv.env['SUPABASE_URL'] ?? '';
-  supabaseKey = dotenv.env['SUPABASE_KEY'] ?? '';
+  // supabaseUrl = dotenv.env['SUPABASE_URL'] ?? '';
+  // supabaseKey = dotenv.env['SUPABASE_KEY'] ?? '';
+
+  supabaseUrl =  io.Platform.environment['SUPABASE_URL'];
+  supabaseKey =  io.Platform.environment['SUPABASE_KEY'];
 }
 
 SupabaseClient initializeSupabase() {
-  return SupabaseClient(supabaseUrl, supabaseKey);
+  return SupabaseClient(supabaseUrl!, supabaseKey!);
 }
